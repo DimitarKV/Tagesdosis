253d7d2662410583b42ef6d068c5f36a32621df7
Stanimir Kolev <stanimirkolev04@gmail.com>
2022-04-20 08:29:11 +0300

    Added TokenController.cs

---
+++ src/Services/Tagesdosis.Services.User/Tagesdosis.Services.User.Api/Controllers/TokenController.cs
@@ -0,0 +1,45 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Tagesdosis.Services.User.DTOs;
+
+namespace Tagesdosis.Services.User.Api.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class TokenController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public TokenController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    [HttpPost]
+    public IActionResult GetToken([FromBody] UserCredentialsDTO credentialsDto)
+    {
+        if (credentialsDto.UserName=="user1" && credentialsDto.Password=="password1")
+        {
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var securityKey = new SymmetricSecurityKey
+                (Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, 
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(issuer: issuer,
+                audience: audience,
+                signingCredentials: credentials);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var stringToken = tokenHandler.WriteToken(token);
+
+            return Ok(stringToken);
+        }
+
+        return Unauthorized();
+    }
+}
---
+++ src/Services/Tagesdosis.Services.User/Tagesdosis.Services.User/DTOs/UserCredentialsDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Tagesdosis.Services.User.DTOs;
+
+public class UserCredentialsDTO
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
