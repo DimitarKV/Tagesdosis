4ec0ee4611818755d661f8d178d9b1c8753bb3dc
Stanimir Kolev <stanimirkolev04@gmail.com>
2022-04-20 07:57:13 +0300

    Added DbContext

---
+++ src/Services/Tagesdosis.Services.User/Tagesdosis.Services.User/Persistence/UserDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tagesdosis.Services.User.Persistence;
+
+public class UserDbContext : DbContext
+{
+    public UserDbContext()
+    {
+        
+    }
+    
+    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+    {
+        
+    }
+}
