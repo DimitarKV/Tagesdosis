82598ba7923f2bae4193ce75846a8e1deea54a10
BatMitio <mitko_zlia@abv.bg>
2022-04-23 18:50:59 +0300

    Started Post microservice development and added post entity and dbcontext

---
+++ src/Services/Tagesdosis.Services.Posts/Data/Persistance/PostDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tagesdosis.Services.Posts.Data.Persistance;
+
+public class PostDbContext : DbContext
+{
+    protected PostDbContext()
+    {
+    }
+
+    public PostDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
